22269e507fda0bf6c34f0808814eb286d09705dd
XiongJian <420048248@qq.com>
2014-07-01 16:25:39 +0800

    by Static
    pageControl + scrollView test

--- WWW/FiveViewController.m
+++ WWW/FiveViewController.m
@@ -8,6 +8,7 @@
 
 #import "FiveViewController.h"
 #import <QuartzCore/QuartzCore.h>
+#import "BusinessUtil.h"
 
 @interface FiveViewController ()
 
@@ -18,7 +19,7 @@
 
 -(void)initArray
 {
-    imageArray = [NSMutableArray arrayWithObjects:[UIImage imageNamed:@"image0.png"],[UIImage imageNamed:@"image1.png"],[UIImage imageNamed:@"image2.png"],[UIImage imageNamed:@"image3.png"],nil];
+    imageArray = [[NSMutableArray alloc]initWithObjects:[UIImage imageNamed:@"image0.png"],[UIImage imageNamed:@"image1.png"],[UIImage imageNamed:@"image2.png"],[UIImage imageNamed:@"image3.png"],nil];
     //存放图片的数组
 
 }
@@ -31,32 +32,32 @@
 -(void)configScrollView
 {
 
-     //初始化UIScrollView，设置相关属性，均可在storyBoard中设置
-     CGRect frame=CGRectMake(0, 0, 320, 180);
-     self.myScrollView = [[UIScrollView alloc]initWithFrame:frame];    //scrollView的大小
-     self.myScrollView.backgroundColor=[UIColor blueColor];
-     self.myScrollView.pagingEnabled=YES;//以页为单位滑动，即自动到下一页的开始边界
-     self.myScrollView.showsVerticalScrollIndicator=NO;
-     self.myScrollView.showsHorizontalScrollIndicator=NO;//隐藏垂直和水平显示条
+    //初始化UIScrollView，设置相关属性，均可在storyBoard中设置
+    CGRect frame = CGRectMake(0, IOS_VERSION>=7?20:0, 320, 180);
+    self.myScrollView = [[UIScrollView alloc]initWithFrame:frame];    //scrollView的大小
+    self.myScrollView.backgroundColor = [UIColor blueColor];
+    self.myScrollView.pagingEnabled = YES;//以页为单位滑动，即自动到下一页的开始边界
+    self.myScrollView.showsVerticalScrollIndicator = NO;
+    self.myScrollView.showsHorizontalScrollIndicator = NO;//隐藏垂直和水平显示条
+    self.myScrollView.delegate = self;
 
-    self.myScrollView.delegate=self;
-    UIImageView *firstView=[[UIImageView alloc] initWithImage:[imageArray lastObject]];
-    CGFloat Width=self.myScrollView.frame.size.width;
-    CGFloat Height=self.myScrollView.frame.size.height;
-    firstView.frame=CGRectMake(0, 0, Width, Height);
-    [self.myScrollView addSubview:firstView];
     //set the last as the first
+    UIImageView *firstView = [[UIImageView alloc] initWithImage:[imageArray lastObject]];
+    CGFloat Width = self.myScrollView.frame.size.width;
+    CGFloat Height = self.myScrollView.frame.size.height;
+    firstView.frame = CGRectMake(0, 0, Width, Height);
+    [self.myScrollView addSubview:firstView];
 
-    for (int i=0; i<[imageArray count]; i++) {
+    for (int i = 0; i < [imageArray count]; i++) {
         UIImageView *subViews=[[UIImageView alloc] initWithImage:[imageArray objectAtIndex:i]];
         subViews.frame=CGRectMake(Width*(i+1), 0, Width, Height);
         [self.myScrollView addSubview: subViews];
     }
 
-    UIImageView *lastView=[[UIImageView alloc] initWithImage:[imageArray objectAtIndex:0]];
-    lastView.frame=CGRectMake(Width*(imageArray.count+1), 0, Width, Height);
-    [self.myScrollView addSubview:lastView];
     //set the first as the last
+    UIImageView *lastView = [[UIImageView alloc] initWithImage:[imageArray objectAtIndex:0]];
+    lastView.frame = CGRectMake(Width*(imageArray.count+1), 0, Width, Height);
+    [self.myScrollView addSubview:lastView];
 
     [self.myScrollView setContentSize:CGSizeMake(Width*(imageArray.count+2), Height)];
     [self.view addSubview:self.myScrollView];
@@ -65,11 +66,11 @@
 
 
      //设置pageControl的位置，及相关属性，可选
-     CGRect pageControlFrame=CGRectMake(100, 160, 78, 36);
-     self.pageControl=[[UIPageControl alloc]initWithFrame:pageControlFrame];
+    CGRect pageControlFrame=CGRectMake(100, IOS_VERSION>=7?170:160, 78, 36);
+    self.pageControl=[[UIPageControl alloc]initWithFrame:pageControlFrame];
 
-     [self.pageControl setBounds:CGRectMake(0, 0, 16*(self.pageControl.numberOfPages-1), 16)];//设置pageControl中点的间距为16
-     [self.pageControl.layer setCornerRadius:8];//设置圆角
+    [self.pageControl setBounds:CGRectMake(0, 0, 16*(self.pageControl.numberOfPages-1), 16)];//设置pageControl中点的间距为16
+    [self.pageControl.layer setCornerRadius:8];//设置圆角
 
     self.pageControl.numberOfPages=imageArray.count;
     //    self.pageControl.backgroundColor=[UIColor blueColor];//背景
@@ -90,6 +91,7 @@
     CGRect rect=CGRectMake((pageNum+2)*viewSize.width, 0, viewSize.width, viewSize.height);
     [self.myScrollView scrollRectToVisible:rect animated:NO];
     pageNum++;
+
     if (pageNum==imageArray.count) {
         CGRect newRect=CGRectMake(viewSize.width, 0, viewSize.width, viewSize.height);
         [self.myScrollView scrollRectToVisible:newRect animated:NO];
@@ -146,7 +148,8 @@
     CGSize viewSize=self.myScrollView.frame.size;
     [self.myScrollView setContentOffset:CGPointMake((pageNum+1)*viewSize.width, 0)];
     NSLog(@"myscrollView.contentOffSet.x==%f",myScrollView.contentOffset.x);
-    NSLog(@"pageControl currentPage==%d",self.pageControl.currentPage);   [myTimer invalidate];
+    NSLog(@"pageControl currentPage==%d",self.pageControl.currentPage);
+    [myTimer invalidate];
 }
 
 @end
